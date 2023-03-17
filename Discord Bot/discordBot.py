2eed03963ca975e2231dd64d40e79cdd68af7e1d
Austin Little <imaustinlittle@gmail.com>
2023-03-17 15:43:36 -0400

    Added shutdown command

--- Discord Bot/discordBot.py
+++ Discord Bot/discordBot.py
@@ -1,9 +1,11 @@
+import sys
 import discord
 from discord.ext import commands
 
 # Create a new bot instance
 intents = discord.Intents.default()
 intents.message_content = True
+intents.application_commands = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Event: on_ready
@@ -16,6 +18,20 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello! I'm your Discord bot!")
 
+# Command: !shutdown
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("Shutting down the bot...")
+    await bot.logout()
+    sys.exit(0)
+
+
+
+
+
 # Run the bot
 TOKEN = "TOKEN"
 bot.run(TOKEN)
+
+
