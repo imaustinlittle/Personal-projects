47c386ec4e11c18268ef200035bd2342a52ab05d
Austin Little <imaustinlittle@gmail.com>
2023-03-20 07:51:12 -0400

    forgot something.

--- Discord Bot/discordBot.py
+++ Discord Bot/discordBot.py
@@ -7,6 +7,7 @@ from discord import Member, Status
 intents = discord.Intents.default()
 intents.message_content = True
 intents.application_commands = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Event: on_ready
